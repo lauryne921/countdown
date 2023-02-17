71ac2786288079f0eb92589b2c8431fff7c529e9
Lauryne <104195191+lauryne921@users.noreply.github.com>
2023-02-17 15:18:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Date cible
-var countDownDate = new Date("2023-02-27T00:00:00Z").getTime();
+let countDownDate = new Date("2023-02-27T00:00:00Z").getTime();
 
 // Mise à jour de la durée toutes les secondes
-var x = setInterval(function() {
+let x = setInterval(function() {
 
   // Date actuelle
-  var now = new Date().getTime();
+  let now = new Date().getTime();
   
   // Calcul de la différence entre les dates
-  var distance = countDownDate - now;
+  let distance = countDownDate - now;
   
   // Calcul des jours, heures, minutes et secondes restants
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
   // Affichage des résultats dans les éléments HTML correspondants
   document.getElementById("days").innerHTML = days;
